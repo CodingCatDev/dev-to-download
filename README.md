b2673c0a572368d46cf778f8fc568f5fbd1602a4
Alex Patterson <alex@codingcat.dev>
2022-07-20 14:34:27 -0400

    initial

---
+++ README.md
@@ -0,0 +1,16 @@
+# Getting started
+
+Clone the repo, install all dependencies, and create `content/` directory in project root. The files will go in there
+
+```
+git clone https://github.com/codercatdev/dev-to-download.git
+cd dev-to-download
+npm install
+
+mv .env.template .env
+add keys to .env
+// replace {YOUR_DEV_KEY} with your dev key values from the https://dev.to/settings/extensions 
+```
+# Usage
+
+ `npm run start`. 
---
+++ util/directories.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const path = require("path");
+
+const mkDirByPathSync = (targetDir, { isRelativeToScript = false } = {}) => {
+  const sep = path.sep;
+  const initDir = path.isAbsolute(targetDir) ? sep : "";
+  const baseDir = isRelativeToScript ? `${__dirname}/../` : ".";
+
+  return targetDir.split(sep).reduce((parentDir, childDir) => {
+    const curDir = path.resolve(baseDir, parentDir, childDir);
+    try {
+      fs.mkdirSync(curDir);
+    } catch (err) {
+      if (err.code === "EEXIST") {
+        // curDir already exists!
+        return curDir;
+      }
+
+      // To avoid `EISDIR` error on Mac and `EACCES`-->`ENOENT` and `EPERM` on Windows.
+      if (err.code === "ENOENT") {
+        // Throw the original parentDir error on curDir `ENOENT` failure.
+        throw new Error(`EACCES: permission denied, mkdir '${parentDir}'`);
+      }
+
+      const caughtErr = ["EACCES", "EPERM", "EISDIR"].indexOf(err.code) > -1;
+      if (!caughtErr || (caughtErr && curDir === path.resolve(targetDir))) {
+        throw err; // Throw if it's just the last created dir.
+      }
+    }
+
+    return curDir;
+  }, initDir);
+};
+const dirCleanCreate = (contentDir) => {
+  if (fs.existsSync(contentDir)) {
+    fs.readdir(contentDir, (err, files) => {
+      if (err) throw err;
+
+      for (const file of files) {
+        fs.unlink(path.join(contentDir, file), (err) => {
+          if (err) throw err;
+        });
+      }
+    });
+  } else {
+    mkDirByPathSync(`${__dirname}/..${contentDir}`, {
+      isRelativeToScript: true,
+    });
+  }
+};
+exports.dirCleanCreate = dirCleanCreate;
